6ae2c7fd29e525c064282a61a7e82d1ee0208c9c
Ben <BenElla91@gmail.com>
2016-05-19 12:11:03 +0300

    benben is great

---
+++ app/src/main/java/ben/playground/Run_parameters.java
@@ -0,0 +1,13 @@
+package ben.playground;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Run_parameters extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_run_parameters);
+    }
+}
